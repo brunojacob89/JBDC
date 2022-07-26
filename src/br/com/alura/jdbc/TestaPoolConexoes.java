7e91e04d6c3413efd53962bba51ad21473992eb1
Bruno Jacob <brunojacob_89@hotmail.com>
2022-07-26 15:53:58 -0300

    atualizando

--- src/br/com/alura/jdbc/TestaPoolConexoes.java
+++ src/br/com/alura/jdbc/TestaPoolConexoes.java
@@ -1,10 +1,19 @@
 package br.com.alura.jdbc;
 
+import java.sql.SQLException;
+import java.util.Iterator;
+
 public class TestaPoolConexoes {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException {
 
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		
+		for (int i = 1; i < 20; i++) {
+			connectionFactory.recuperaConexao();
+			System.out.println("Conexão de numero " + i);
+		}
+		
 	}
 
 }
